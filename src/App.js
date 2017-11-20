3089455ae9a8d1638f1cd3342b2042dc0d9aa081
Mariley20 <mariley2018@gmail.com>
2017-11-19 23:56:28 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {SignIn} from './SignIn';
 import {SignUp} from './SignUp';
 import {MyBoards} from './MyBoards';
 import {Details} from './BoardDetail';
-const App = ({myBoard, newBoard, selected, successLogin}) => {
+const App = ({myBoard, newBoard, selected, successLogin, user}) => {
 return (
     <div className="container">
         <Header />
@@ -21,11 +21,12 @@ return (
         </div> */}
         <BrowserRouter>
             <Switch>
-                    <Route exact path="/" render={() => <SignIn successLogin={successLogin} />}/>
+                    <Route name="home"  exact path = "/home" render={() => <Home successLogin={successLogin} user={user}/>}/>
                     <Route path="/sign_up" render={() => <SignUp successLogin={successLogin} />}/>
                     <Route path="/sign_in"  render={() => <SignIn successLogin={successLogin} />}/>
                     <Route path="/myboard"  render={() => <MyBoards myBoard={myBoard} newBoard={newBoard} selected={selected} />}/>
                     <Route path="/details"  render={() => <Details myBoard={myBoard}  selected={selected} />}/>
+                    <Route exact path="/" render={() => <SignIn successLogin={successLogin} />}/>
                     <Route path='/trello-redux' render={() => <Redirect to="/sign_in"/>}/>
             </Switch>
         </BrowserRouter>
@@ -39,6 +40,6 @@ const Header = () => {
         </div>
     )
 }
-const mapToProps = ({myBoard, newBoard, selected, successLogin}) => ({myBoard, newBoard, selected, successLogin});
+const mapToProps = ({myBoard, newBoard, selected, successLogin, user}) => ({myBoard, newBoard, selected, successLogin, user});
 
 export default connect(mapToProps)(App);
---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+// import { Button, Form, FormControl, FormGroup, Col, Checkbox } from
+// 'react-bootstrap';
+import {NavLink, Redirect} from 'react-router-dom';
+import {signIn, signOut, signUp} from './actionsLogin'
+export const Home  = ({successLogin, user}) => {
+    return (
+      <div className="App">
+         {
+            !successLogin  && <Redirect to = "/login" />
+         }
+         <h1> Kahoot Survey Admin </h1>
+         <button onClick = {signOut}>
+            SignOut
+         </button>   
+            <div>
+                 {user.email} - {user.fullname} - {user.survey} -  {user.question} - {user.options} 
+            </div>
+            <Redirect to = "/myboard" />
+      </div>
+    );
+}
